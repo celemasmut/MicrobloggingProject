45cce9494520a484fa227f37e970fc9a920a7f2f
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2021-01-12 11:25:20 -0300

    Configurations + BD

---
+++ bd/conexionBD.go
@@ -0,0 +1,38 @@
+package bd
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+/*MongoCN es el objeto de conexión a la base de datos*/
+var MongoCN = ConectarBD()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://microblogging:rcm4741695@cluster0.3twrc.mongodb.net/microbloggingGo?retryWrites=true&w=majority")
+
+/*ConectarBD es la función que me permite conectar a la base de datos*/
+func ConectarBD() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Println("Conexión exitosa con la BD")
+	return client
+}
+
+/*ChequeoConnection es el ping a la BD*/
+func ChequeoConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
