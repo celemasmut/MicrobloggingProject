6ba05fa23b6a7f885aa977beec7687b1abaec182
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2021-01-15 22:47:22 -0300

    cambios en bson-usuario

--- models/usuario.go
+++ models/usuario.go
@@ -9,14 +9,14 @@ import (
 //Usuario es el model de usuario de la BD de mongoDB
 type Usuario struct {
 	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Nombre          string             `bson:"_nombre" json:"nombre,omitempty"`
-	Apellidos       string             `bson:"_apellidos" json:"apellidos,omitempty"`
-	FechaNacimiento time.Time          `bson:"_fechaNacimiento" json:"fechaNacimiento,omitempty"`
-	Email           string             `bson:"_email" json:"email"`
-	Password        string             `bson:"_password" json:"password,omitempty"`
-	Avatar          string             `bson:"_avatar" json:"avatar,omitempty"`
-	Banner          string             `bson:"_banner" json:"banner,omitempty"`
-	Biografia       string             `bson:"_biografia" json:"biografia,omitempty"`
-	Ubicacion       string             `bson:"_ubicacion" json:"ubicacion,omitempty"`
-	SitioWeb        string             `bson:"_sitioweb" json:"sitioweb,omitempty"`
+	Nombre          string             `bson:"nombre" json:"nombre,omitempty"`
+	Apellidos       string             `bson:"apellidos" json:"apellidos,omitempty"`
+	FechaNacimiento time.Time          `bson:"fechaNacimiento" json:"fechaNacimiento,omitempty"`
+	Email           string             `bson:"email" json:"email"`
+	Password        string             `bson:"password" json:"password,omitempty"`
+	Avatar          string             `bson:"avatar" json:"avatar,omitempty"`
+	Banner          string             `bson:"banner" json:"banner,omitempty"`
+	Biografia       string             `bson:"biografia" json:"biografia,omitempty"`
+	Ubicacion       string             `bson:"ubicacion" json:"ubicacion,omitempty"`
+	SitioWeb        string             `bson:"sitioweb" json:"sitioweb,omitempty"`
 }
