803ea2ac45c4002cea4cda0013ba463c53d27e0a
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2021-01-24 19:22:44 -0300

    insertoTweet

---
+++ bd/insertoTweet.go
@@ -0,0 +1,32 @@
+package bd
+
+import (
+	"context"
+	"time"
+
+	"github.com/celemasmut/MicrobloggingProject/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+//InsertoTweet graba el Tweet en la BD
+func InsertoTweet(usu models.GraboTweet) (string, bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("microdb")
+	col := db.Collection("tweet")
+
+	registro := bson.M{
+		"usserid": usu.UserId,
+		"mensaje": usu.Mensaje,
+		"fecha":   usu.Fecha,
+	}
+
+	result, err := col.InsertOne(ctx, registro)
+	if err != nil {
+		return "", false, err
+	}
+	objID, _ := result.InsertedID.(primitive.ObjectID)
+	return objID.String(), true, nil
+}
